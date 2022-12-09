ed0107b3ab6d182b5e2aca03d9a4ea4d0f5382f0
Iris Shi <113374389+Riri200@users.noreply.github.com>
2022-12-09 14:03:43 -0800

    Update experiment.py

--- experiment.py
+++ experiment.py
@@ -30,8 +30,8 @@ expInfo['date']  = datetime.now()
 filename = (str(expInfo['Subject_Name'])+'_'+(expInfo['Age'])+'_'+(expInfo['Gender'])+ '_Results.csv')
 print(filename)
 
-anxietyText = visual.TextStim(win, text='Before the experiment begins, I would like you to imagine yourself standing on a rooftop that is very high off the ground. The rooftop is very narrow and slippery with no railings. One misstep and you will fall...!')
-instrucText = visual.TextStim(win, text='You will now solve mathematic equations as fast and as accurate as possible. Press any key to begin block')
+anxietyText = visual.TextStim(win, text='Before the experiment begins, imagine standing on a rooftop that is very high off the ground. The rooftop is very narrow and slippery with no railings. One misstep and you will fall...! Press any key to proceed.')
+instrucText = visual.TextStim(win, text='You will now solve mathematic equations as fast and as accurate as possible. Press any key to begin')
 fixation = visual.TextStim(win, text='+', color='black')
 
 nTrials=10
